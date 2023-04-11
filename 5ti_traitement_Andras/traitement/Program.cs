b235de5842a0a41b261bb243b9833f2748cd204d
Andras Corda <115708078+R3tr0-andras@users.noreply.github.com>
2023-04-11 18:18:00 +0200

    Add files via upload

---
+++ 5ti_traitement_Andras/traitement/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace traitement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nombre, resultat;
+            int choix;
+
+            do
+            {
+                Console.Clear();
+                // Demander les dimensions des matrices
+                Console.WriteLine("Entrez le nombre de lignes de la première matrice :");
+                int rows1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Entrez le nombre de colonnes de la première matrice :");
+                int cols1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Entrez le nombre de lignes de la deuxième matrice :");
+                int rows2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Entrez le nombre de colonnes de la deuxième matrice :");
+                int cols2 = int.Parse(Console.ReadLine());
+
+                // Générer les nombres aléatoires pour les matrices
+                Random rand = new Random();
+                int[,] matrix1 = new int[rows1, cols1];
+                int[,] matrix2 = new int[rows2, cols2];
+                for (int i = 0; i < rows1; i++)
+                {
+                    for (int j = 0; j < cols1; j++)
+                    {
+                        matrix1[i, j] = rand.Next(10);
+                    }
+                }
+                for (int i = 0; i < rows2; i++)
+                {
+                    for (int j = 0; j < cols2; j++)
+                    {
+                        matrix2[i, j] = rand.Next(10);
+                    }
+                }
+
+                Console.WriteLine("| Choisissez une méthode de traitement :");
+                Console.WriteLine("| 1. Concaténation");
+                Console.WriteLine("| 2. Addition");
+                Console.WriteLine("| 3. multiplication");
+                choix = Convert.ToInt32(Console.ReadLine());
+
+                switch (choix)
+                {
+                    case 1:
+                        Console.Clear();
+                        traitement.afficher();
+                        traitement.ConcatenateMatrices();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        traitement.afficher();
+                        traitement.AddMatrices();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        traitement.afficher();
+                        traitement.MultiplyMatrices();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Erreur : La réponse est 42 !");
+                        break;
+                }
+
+                Console.Write("Voulez-vous recommencer ? (O/N) : ");
+            } while (Console.ReadLine().ToUpper() == "O");
+
+            Console.WriteLine("Au revoir !");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 5ti_traitement_Andras/traitement/traitement.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace traitement
+{
+    public struct traitement
+    {
+        public void afficher(int rows1, int cols1, int rows2, int cols2, out int[,] matrix1, int[,] matrix2)
+        {
+            // Afficher les matrices générées
+            Console.WriteLine("Première matrice :");
+            for (int i = 0; i < rows1; i++)
+            {
+                for (int j = 0; j < cols1; j++)
+                {
+                    Console.Write(matrix1[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Deuxième matrice :");
+            for (int i = 0; i < rows2; i++)
+            {
+                for (int j = 0; j < cols2; j++)
+                {
+                    Console.Write(matrix2[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        
+        public int[,] AddMatrices(int[,] matrix1, int[,] matrix2)
+        {
+            int rows1 = matrix1.GetLength(0);
+            int cols1 = matrix1.GetLength(1);
+            int rows2 = matrix2.GetLength(0);
+            int cols2 = matrix2.GetLength(1);
+
+            // Vérifier que les deux matrices ont la même dimension
+            if (rows1 != rows2 || cols1 != cols2)
+            {
+                throw new ArgumentException("Les deux matrices doivent avoir la même dimension.");
+            }
+
+            // Créer la matrice résultat
+            int[,] resultMatrix = new int[rows1, cols1];
+
+            // Additionner les éléments de chaque matrice pour créer la matrice résultat
+            for (int i = 0; i < rows1; i++)
+            {
+                for (int j = 0; j < cols1; j++)
+                {
+                    resultMatrix[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+
+            // Retourner la matrice résultat
+            return resultMatrix;
+        }
+
+        public int[,] ConcatenateMatrices(int[,] matrix1, int[,] matrix2)
+        {
+        int rows1 = matrix1.GetLength(0);
+        int cols1 = matrix1.GetLength(1);
+        int rows2 = matrix2.GetLength(0);
+        int cols2 = matrix2.GetLength(1);
+
+        // Vérifier que les deux matrices ont le même nombre de lignes
+        if (rows1 != rows2)
+        {
+            throw new ArgumentException("Les deux matrices doivent avoir le même nombre de lignes.");
+        }
+
+        // Créer la matrice résultat
+        int[,] resultMatrix = new int[rows1, cols1 + cols2];
+
+        // Copier les éléments de la première matrice dans la matrice résultat
+        for (int i = 0; i < rows1; i++)
+        {
+            for (int j = 0; j < cols1; j++)
+            {
+                resultMatrix[i, j] = matrix1[i, j];
+            }
+        }
+
+        // Copier les éléments de la deuxième matrice dans la matrice résultat
+        for (int i = 0; i < rows2; i++)
+        {
+            for (int j = 0; j < cols2; j++)
+            {
+                resultMatrix[i, j + cols1] = matrix2[i, j];
+            }
+        }
+
+        // Retourner la matrice résultat
+        return resultMatrix;
+        }
+
+        static int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2)
+        {
+            int rows1 = matrix1.GetLength(0);
+            int cols1 = matrix1.GetLength(1);
+            int rows2 = matrix2.GetLength(0);
+            int cols2 = matrix2.GetLength(1);
+
+            // Vérifier que le nombre de colonnes de la première matrice est égal au nombre de lignes de la deuxième matrice
+            if (cols1 != rows2)
+            {
+                throw new ArgumentException("Le nombre de colonnes de la première matrice doit être égal au nombre de lignes de la deuxième matrice.");
+            }
+
+            // Créer la matrice résultat
+            int[,] resultMatrix = new int[rows1, cols2];
+
+            // Multiplier les matrices pour créer la matrice résultat
+            for (int i = 0; i < rows1; i++)
+            {
+                for (int j = 0; j < cols2; j++)
+                {
+                    for (int k = 0; k < cols1; k++)
+                    {
+                        resultMatrix[i, j] += matrix1[i, k] * matrix2[k, j];
+                    }
+                }
+            }
+
+            // Retourner la matrice résultat
+            return resultMatrix;
+        }
+    }
+}
